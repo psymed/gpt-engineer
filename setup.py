4a5d749528cb322101dad14dfe2f99279e6a5d36
psymed <shailelchuk@gmail.com>
2023-06-25 13:31:21 +0300

    Create setup.py
    This file tells Python how to install the gpt-engineer code as a library.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gpt-engineer',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List the packages that gpt-engineer depends on
+        # For example: 'openai', 'flask', etc.
+    ],
+)
